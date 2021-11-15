4b53724b55e69e1760c187fc16e5fc1901f6dece
SaraAmd <SaraAmd@vt.edu>
2021-11-15 17:17:42 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Semi-Supervised-Learning
+This repository contains the deep semi-supervsised learning on CIFAR-10 dataset condifrtking out of distribution data points (OOD)
+in unlabeled data.
---
+++ ssl_lib/models/builder.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# from .resnet import ResNet
+# from .shakenet import ShakeNet
+# from .cnn13 import CNN13
+from .resnet3 import WideResNet
+
+# WRN-n-k denotes a residual network that has a total number of convolutional layers
+# n and a widening factor k
+def gen_model(name, num_classes, img_size):
+    # scale =  int(np.ceil(np.log2(img_size)))
+    if name == "wrn":
+        return WideResNet(num_classes,depth=28,widen_factor=2)
+    else:
+        raise NotImplementedError
